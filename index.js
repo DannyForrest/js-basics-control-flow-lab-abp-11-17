248bb1d1ccb3aa8f9f7f0b54d82da253d38112b1
Danny Forrest <dannyforrest@gmail.com>
2017-11-10 13:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function ternaryCheckCity(destination) {
 
 function switchOnCharmFromTip(tip) {
   switch(true) {
-    case tip === "generous"
-      return "Thank you so much.";
+    case tip === "generous":
+      return "Thank you so much."
   }
 }
